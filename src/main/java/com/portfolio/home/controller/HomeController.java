b5e099a66ad7eb0f80e84a4f1760142c606de410
dbsdlfud <99ysl@naver.com>
2022-07-15 19:27:17 +0900

    homeController 기본 설정

---
+++ src/main/java/com/portfolio/home/controller/HomeController.java
@@ -0,0 +1,10 @@
+package com.portfolio.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+}
